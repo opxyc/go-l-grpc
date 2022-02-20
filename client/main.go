9b1bf72dca79841c7966361f4227401d3948ed96
opxyc <emmanuvelbony@gmail.com>
2022-02-20 16:00:59 +0530

    identify client

--- client/main.go
+++ client/main.go
@@ -9,17 +9,39 @@ import (
 	"google.golang.org/grpc/credentials"
 )
 
+type Authentication struct {
+	Login    string
+	Password string
+}
+
+// GetRequestMetadata gets the current request metadata
+func (a *Authentication) GetRequestMetadata(context.Context, ...string) (map[string]string, error) {
+	return map[string]string{
+		"login":    a.Login,
+		"password": a.Password,
+	}, nil
+}
+
+// RequireTransportSecurity indicates whether the credentials requires transport security
+func (a *Authentication) RequireTransportSecurity() bool {
+	return true
+}
+
 func main() {
 	creds, err := credentials.NewClientTLSFromFile("cert/server/server.crt", "")
 	if err != nil {
 		log.Fatalf("could not load certificate: %v", err)
 	}
 
-	conn, err := grpc.Dial(":7777", grpc.WithTransportCredentials(creds))
+	auth := &Authentication{
+		Login:    "john",
+		Password: "doe",
+	}
+
+	conn, err := grpc.Dial(":7777", grpc.WithTransportCredentials(creds), grpc.WithPerRPCCredentials(auth))
 	if err != nil {
 		log.Fatalf("could not connect: %v", err)
 	}
-
 	defer conn.Close()
 
 	c := api.NewPingClient(conn)
